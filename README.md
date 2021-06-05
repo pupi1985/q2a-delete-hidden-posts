1ca2e75a26d5b90b9ad190e64219fa6863bac0df
pupi1985 <pupi1985@bitbucket.org>
2021-06-05 17:59:53 -0300

    Reformat code

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
 # Delete Hidden Posts
 
-This is a plugin for [Question2Answer](https://github.com/q2a/question2answer) websites that deletes all hidden posts with/with-out having children posts  
+This is a plugin for [Question2Answer](https://github.com/q2a/question2answer) websites that deletes all hidden posts
+with/with-out having children posts
 
-The latest version of Question2Answer does not allow to delete the posts directly which have some children posts (the comments and answers for a question are said to be children posts for that question and the comments to a answer are said to be children posts to a answer ). You must have to hide and then delete all its child posts one by one before you delete the parent one. This applies to both the Quesions and answers as well. 
+The latest version of Question2Answer does not allow to delete the posts directly which have some children posts (the
+comments and answers for a question are said to be children posts for that question and the comments to a answer are
+said to be children posts to a answer ). You must have to hide and then delete all its child posts one by one before you
+delete the parent one. This applies to both the Quesions and answers as well.
 
-This plugin adds a Delete button right after the question / answer / comment (can be controlled via admin panel options) which can delete the posts (question / answer / comment) with a single click. 
+This plugin adds a Delete button right after the question / answer / comment (can be controlled via admin panel options)
+which can delete the posts (question / answer / comment) with a single click.
 
-Also if your website has many hidden posts which have many child posts this plugin can delete them with one click effort.
+Also if your website has many hidden posts which have many child posts this plugin can delete them with one click
+effort.
 
 ## Features
 
-- Ability to delete all the hidden posts from Question2Answer database having dependencies 
-- When a question is deleted all its Answers, Comments gets deleted 
-- When a answer is deleted all its Comments and Related Questions gets deleted 
-- Buttons are displayed *after* a Question's or Answer's or Comemnt's response buttons - which helps to delete the post with one click
+- Ability to delete all the hidden posts from Question2Answer database having dependencies
+- When a question is deleted all its Answers, Comments gets deleted
+- When a answer is deleted all its Comments and Related Questions gets deleted
+- Buttons are displayed *after* a Question's or Answer's or Comemnt's response buttons - which helps to delete the post
+  with one click
 
 ## Version History
 
@@ -21,14 +28,15 @@ Also if your website has many hidden posts which have many child posts this plug
     * added ask user confirmation while deleting a question / answer / comment
     * added ask admin confirmation while deleting all hidden posts
     * removed css styling , only handling with q2a provided classes
-    * improved redirect logic 
-    * improved code 
+    * improved redirect logic
+    * improved code
 - 1.0
-    * initial stable release 
+    * initial stable release
 
 ## Disclaimer
 
-This is **beta** code.  It is probably okay for production environments, but may not work exactly as expected.  Refunds will not be given.  If it breaks, you get to keep both parts.
+This is **beta** code. It is probably okay for production environments, but may not work exactly as expected. Refunds
+will not be given. If it breaks, you get to keep both parts.
 
 ## About Question2Answer
 
--- qa-dhp-admin.php
+++ qa-dhp-admin.php
@@ -1,94 +1,93 @@
 <?php
 
-    class qa_dhp_admin
+class qa_dhp_admin
+{
+
+    const SAVE_BTN = 'ami_dhp_save_button';
+    const DELETE_HIDDEN_POSTS_BTN = 'ami_dhp_delete_button';
+
+    function option_default($option)
     {
-
-        const SAVE_BTN                = 'ami_dhp_save_button';
-        const DELETE_HIDDEN_POSTS_BTN = 'ami_dhp_delete_button';
-
-        function option_default($option)
-        {
-            switch ($option) {
-                case AMI_DHP_Constants::PLUGIN_ENABLED :
-                case AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA :
-                    return 1;
-                    break;
-                case AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q :
-                    return QA_USER_LEVEL_ADMIN;
-                default:
-                    return null;
-            }
-        }
-
-        function admin_form(&$qa_content)
-        {
-
-            //	Process form input
-
-            $ok = null;
-            if (qa_clicked(self::SAVE_BTN)) {
-                qa_opt(AMI_DHP_Constants::PLUGIN_ENABLED, (bool) qa_post_text(AMI_DHP_Constants::PLUGIN_ENABLED));
-                qa_opt(AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA, (bool) qa_post_text(AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA));
-                qa_opt(AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q, (int) qa_post_text(AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q));
-                $ok = qa_lang('admin/options_saved');
-            } elseif (qa_clicked(self::DELETE_HIDDEN_POSTS_BTN)) {
-                $ok = dhp_lang('all_hidden_posts_deleted');
-                ami_dhp_delete_hidden_posts_process();
-            }
-
-            //	Create the form for display_header_text();
-
-            $user_levels = array(
-                QA_USER_LEVEL_EXPERT    => qa_lang('users/level_expert'),
-                QA_USER_LEVEL_EDITOR    => qa_lang('users/level_editor'),
-                QA_USER_LEVEL_MODERATOR => qa_lang('users/level_moderator'),
-                QA_USER_LEVEL_ADMIN     => qa_lang('users/level_admin'),
-                QA_USER_LEVEL_SUPER     => qa_lang('users/level_super'),
-            );
-
-            $fields = array();
-
-            $fields[ AMI_DHP_Constants::PLUGIN_ENABLED ] = array(
-                'label' => dhp_lang('dhp_enable'),
-                'tags'  => 'NAME="' . AMI_DHP_Constants::PLUGIN_ENABLED . '" onClick=""',
-                'value' => qa_opt(AMI_DHP_Constants::PLUGIN_ENABLED),
-                'type'  => 'checkbox',
-            );
-
-
-            $fields[ AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA ] = array(
-                'id'    => AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA,
-                'label' => dhp_lang('same_user_can_delete'),
-                'type'  => 'checkbox',
-                'value' => qa_opt(AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA),
-                'tags'  => 'NAME="' . AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA . '"',
-            );
-
-            $fields[ AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q ] = array(
-                'id'      => AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q,
-                'label'   => dhp_lang('choose_who_can_delete_all'),
-                'type'    => 'select',
-                'value'   => $user_levels[ qa_opt(AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q) ],
-                'options' => $user_levels,
-                'tags'    => 'NAME="' . AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q . '"',
-            );
-
-            return array(
-                'ok'      => ($ok) ? $ok : null,
-
-                'fields'  => $fields,
-
-                'buttons' => array(
-                    array(
-                        'label' => qa_lang_html('main/save_button'),
-                        'tags'  => 'NAME="' . self::SAVE_BTN . '"',
-                    ),
-                    array(
-                        'label' => dhp_lang('delete_hidden_posts'),
-                        'tags'  => 'NAME="' . self::DELETE_HIDDEN_POSTS_BTN . '" onclick="dhp_ask_user_confirmation(event) && qa_show_waiting_after(this, false);"',
-                    ),
-
-                ),
-            );
+        switch ($option) {
+            case AMI_DHP_Constants::PLUGIN_ENABLED :
+            case AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA :
+                return 1;
+                break;
+            case AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q :
+                return QA_USER_LEVEL_ADMIN;
+            default:
+                return null;
         }
     }
+
+    function admin_form(&$qa_content)
+    {
+
+        //	Process form input
+
+        $ok = null;
+        if (qa_clicked(self::SAVE_BTN)) {
+            qa_opt(AMI_DHP_Constants::PLUGIN_ENABLED, (bool)qa_post_text(AMI_DHP_Constants::PLUGIN_ENABLED));
+            qa_opt(AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA, (bool)qa_post_text(AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA));
+            qa_opt(AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q, (int)qa_post_text(AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q));
+            $ok = qa_lang('admin/options_saved');
+        } else if (qa_clicked(self::DELETE_HIDDEN_POSTS_BTN)) {
+            $ok = dhp_lang('all_hidden_posts_deleted');
+            ami_dhp_delete_hidden_posts_process();
+        }
+
+        //	Create the form for display_header_text();
+
+        $user_levels = array(
+            QA_USER_LEVEL_EXPERT => qa_lang('users/level_expert'),
+            QA_USER_LEVEL_EDITOR => qa_lang('users/level_editor'),
+            QA_USER_LEVEL_MODERATOR => qa_lang('users/level_moderator'),
+            QA_USER_LEVEL_ADMIN => qa_lang('users/level_admin'),
+            QA_USER_LEVEL_SUPER => qa_lang('users/level_super'),
+        );
+
+        $fields = array();
+
+        $fields[AMI_DHP_Constants::PLUGIN_ENABLED] = array(
+            'label' => dhp_lang('dhp_enable'),
+            'tags' => 'NAME="' . AMI_DHP_Constants::PLUGIN_ENABLED . '" onClick=""',
+            'value' => qa_opt(AMI_DHP_Constants::PLUGIN_ENABLED),
+            'type' => 'checkbox',
+        );
+
+        $fields[AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA] = array(
+            'id' => AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA,
+            'label' => dhp_lang('same_user_can_delete'),
+            'type' => 'checkbox',
+            'value' => qa_opt(AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA),
+            'tags' => 'NAME="' . AMI_DHP_Constants::SAME_USER_CAN_DELETE_QA . '"',
+        );
+
+        $fields[AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q] = array(
+            'id' => AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q,
+            'label' => dhp_lang('choose_who_can_delete_all'),
+            'type' => 'select',
+            'value' => $user_levels[qa_opt(AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q)],
+            'options' => $user_levels,
+            'tags' => 'NAME="' . AMI_DHP_Constants::MIN_LEVEL_TO_DELETE_Q . '"',
+        );
+
+        return array(
+            'ok' => ($ok) ? $ok : null,
+
+            'fields' => $fields,
+
+            'buttons' => array(
+                array(
+                    'label' => qa_lang_html('main/save_button'),
+                    'tags' => 'NAME="' . self::SAVE_BTN . '"',
+                ),
+                array(
+                    'label' => dhp_lang('delete_hidden_posts'),
+                    'tags' => 'NAME="' . self::DELETE_HIDDEN_POSTS_BTN . '" onclick="dhp_ask_user_confirmation(event) && qa_show_waiting_after(this, false);"',
+                ),
+
+            ),
+        );
+    }
+}
